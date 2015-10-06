457037723615d6e6077cdb05ccec5f6f35f0c5e5
Jan Studen√Ω <jendas1@yahoo.com>
2015-10-06 19:53:37 +0200

    Initial Commit

---
+++ Jupyter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Jupyter
+//
+//  Created by Jan Studený on 06/10/15.
+//  Copyright © 2015 Jan Studený. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
