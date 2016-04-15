a8f3d86624b472df087b284de22e6b24bc9949c2
Jan Studený <jendas1@yahoo.com>
2016-04-15 20:40:42 +0200

    Added readme, prepared for github :)

--- Jupyter/AppDelegate.swift
+++ Jupyter/AppDelegate.swift
@@ -12,16 +12,39 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
-
+    
+    @IBOutlet weak var menu: NSMenu!
+    let menuItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength);
+    let jupyter_notebook = NSTask()
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        jupyter_notebook.launchPath = "/usr/local/bin/python3"
+        jupyter_notebook.currentDirectoryPath = NSHomeDirectory()
+        jupyter_notebook.arguments = ["-m", "notebook", "--no-browser", "-y"]
+        jupyter_notebook.environment = ["LC_ALL": "en_US.UTF-8"];
+        let icon = NSImage(named: "menuBarIcon")!
+        icon.template = true
+        menuItem.image = icon
+        menuItem.menu = menu
+        jupyter_notebook.launch()
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    
+    func applicationWillTerminate(notification: NSNotification) {
+        jupyter_notebook.terminate()
     }
-
-
+    
+    func application(sender: NSApplication, openFile filename: String) -> Bool {
+        if filename.rangeOfString(NSHomeDirectory()) == nil {
+            return false;
+        }
+        let relative_filename = filename.stringByReplacingOccurrencesOfString(NSHomeDirectory() + "/", withString: "")
+        let escaped_filename = relative_filename.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())
+        NSWorkspace.sharedWorkspace().openURL(NSURL(string: "http://localhost:8888/notebooks/" + escaped_filename!)!)
+        return true;
+    }
+    
+    @IBAction func quitClicked(sender: NSMenuItem) {
+        NSApplication.sharedApplication().terminate(self)
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,11 @@
+## Jupyter app
+__Simple app for opening jupyter notebooks right from Finder!__
+
+App starts jupyter server on port 8888 with user folder as base folder. Clicking on any .ipynb file will open it in your favorite browser.
+
+### Install
+Just download app from releases tab in github.
+
+Check out also [jupyter-notebook-quicklook-plugin](www.github.com/jendas1/jupyter-notebook-quicklook-plugin).
+
+Enjoy!
